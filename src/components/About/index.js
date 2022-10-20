d62904597f97d89a69d21c5fbb805775de044b41
Brian King <bnk5532@gmail.com>
2022-10-20 18:59:14 -0400

    adjusted css for about component

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,17 +3,22 @@ import familyPic1 from "../../assets/images/familyPic1.png";
 
 function About() {
   return (
-    <div className="mx-3">
+    <div className="container mx-3">
       <h1>About me</h1>
-      <div className="about">
+      <div>
+        
+        <div className="about-pic">
         <img
           className="rounded"
           src={familyPic1}
-          style={{ height: "18rem" }}
+          style={{ height: "25rem" }}
           alt=""
         />
-
+        </div>
+        
+        <div className="about-text">
         <p className="mx-3">
+          <br></br>
           <strong>Brian Nicholas King</strong>
           <br></br>
           <em>Operations Reporting Sr. Analyst & Web Developer</em>
@@ -22,13 +27,14 @@ function About() {
           intel and reporting analytics while working in the telecommunications
           and technology industries, I have taken a recent pause in my career to
           pursue a new one in software engineering, and I am living the
-          "coding-life," as I regularly quips to friends and family. I currently
+          "coding-life," as I regularly quip to friends and family. I currently
           reside in the lower Cape Fear region of NC with my wife,
           Vlasta-like-pasta, and two children, affectionately known as
           Victoria-the-Great, and Brian-the-Terrible. <br></br>
           <br></br>
           <em>I do hope that you enjoy my porfolio!</em>
         </p>
+        </div>
       </div>
     </div>
   );
