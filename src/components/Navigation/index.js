1608df24b80cea78881aff696c4b2eb85ebfa9c1
Brian King <bnk5532@gmail.com>
2022-10-28 08:48:45 -0400

    added collapsable menu to navbar and disabled contact page for now

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,57 +1,54 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import profilePic from "../../assets/images/Profile BNK.jpeg";
 
 function NavBar({ currentCategory, changeSelected }) {
   return (
     <>
-      <Navbar sticky="top" bg="dark" variant="dark">
+      <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark" >
         <Container>
           <Navbar.Brand onClick={() => changeSelected("About")}>
             {" Brian Nicholas King | Portfolio"}
           </Navbar.Brand>
-          <Nav className="justify-content-end">
-            <Nav.Link onClick={() => changeSelected("About")}>About</Nav.Link>
-            <Nav.Link onClick={() => changeSelected("Portfolio")}>
-              Portfolio
-            </Nav.Link>
-            <Nav.Link onClick={() => changeSelected("Resume")}>Resume</Nav.Link>
-            <Nav.Link onClick={() => changeSelected("Contact")}>
-              Contact
-            </Nav.Link>
-            <Nav.Link
-              href="https://github.com/bnk5532"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa fa-github is-size-5" aria-hidden="true"></i>
-            </Nav.Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link onClick={() => changeSelected("About")}>About</Nav.Link>
+              <Nav.Link onClick={() => changeSelected("Portfolio")}>
+                Portfolio
+              </Nav.Link>
+              <Nav.Link onClick={() => changeSelected("Resume")}>
+                Resume
+              </Nav.Link>
+              {/* <Nav.Link onClick={() => changeSelected("Contact")}>
+        Contact
+      </Nav.Link> */}
 
-            <Nav.Link
-              href="https://linkedin.com/in/bnk5532"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-linkedin" aria-hidden="true"></i>
-            </Nav.Link>
+              <Nav.Link
+                href="https://github.com/bnk5532"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa fa-github is-size-5" aria-hidden="true"></i>
+              </Nav.Link>
 
-            <Nav.Link
-              href="https://youtube.com/user/briguy618"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-youtube" aria-hidden="true"></i>
-            </Nav.Link>
-{/*         
-            <img
-              alt=""
-              src={profilePic}
-              width="38"
-              height="38"
-              className="mx-4 d-inline-block rounded"
-            /> */}
-          </Nav>
+              <Nav.Link
+                href="https://linkedin.com/in/bnk5532"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-linkedin" aria-hidden="true"></i>
+              </Nav.Link>
+
+              <Nav.Link
+                href="https://youtube.com/user/briguy618"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-youtube" aria-hidden="true"></i>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
