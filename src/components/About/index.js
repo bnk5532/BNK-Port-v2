fff57a16ca6416efb99f2dc4bcff03ce0bbc74f1
Brian King <bnk5532@gmail.com>
2022-12-05 16:12:00 -0500

    updated about me text, added bootcamp badge to recommendations page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,34 +5,36 @@ function About() {
   return (
     <div className="container">
       <h1>About me</h1>
-      <div style={{width: '100%'}}>
-        
+      <div style={{ width: "100%" }}>
         <div className="about-pic">
-        <img
-          className="picture"
-          src={familyPic1}
-          alt=""
-        />
+          <img className="picture" src={familyPic1} alt="" />
         </div>
-        
+
         <div className="about-text">
-        <p>
-          <br></br>
-          <strong>Brian Nicholas King</strong>
-          <br></br>
-          <em>Operations Reporting Sr. Analyst & Web Developer</em>
-          <br></br>
-          <br></br>With over 13 years of professional experience in business
-          intel and reporting analytics while working in the telecommunications
-          and technology industries, I have taken a recent pause in my career to
-          pursue a new one in software engineering, and I am living the
-          "coding-life," as I regularly quip to friends and family. I currently
-          reside in the lower Cape Fear region of NC with my wife,
-          Vlasta-like-pasta, and two children, affectionately known as
-          Victoria-the-Great, and Brian-the-Terrible. <br></br>
-          <br></br>
-          <em>I do hope that you enjoy my portfolio!</em>
-        </p>
+          <p>
+            <br></br>
+            <strong>Brian Nicholas King</strong>
+            <br></br>
+            <em>Operations Reporting Sr. Analyst & Web Developer</em>
+            <br></br>
+            <br></br>Brian is excited and proud to share that he has survived
+            the rigors of the coding bootcamp through the University of North
+            Carolina - Chapel Hill and is a newly minted web developer. Having
+            spent the past 13 years of his professional career in business intel
+            and reporting analytics while working in the telecommunications and
+            technology industries, he had recently taken a pause in his career
+            to pursue a new one in software engineering. Over the past 6 months,
+            he has learned the the tech industry's wishlist of languages,
+            skills, tools and the problem-solver mindset that is critical to
+            successfully landing and maintaining a web developer career.
+            <br></br> <br></br>
+            Coding camp in the rear view, Brian is now looking forward to 2023,
+            and the job hunt for a new role as a Junior Web Developer!
+            <br></br> <br></br>Brian calls the lower Cape Fear region of North
+            Carolina home where he shares a beautiful home with his wife and two elementary school aged children, and yorkshire terrier.
+            <br></br>
+            <br></br>
+          </p>
         </div>
       </div>
     </div>
--- src/components/Recommendations/index.js
+++ src/components/Recommendations/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import codeBadge1 from "../../assets/images/codeBadge1.png"
 
 function Recommendations() {
   return (
@@ -6,8 +7,23 @@ function Recommendations() {
       <h1>Recommendations</h1>
 
       <div className="recommendations">
+        "Brian stood out in many ways as a student, but a couple of things that
+        really set him apart were his perfect attendance and the fact that he
+        consistently arrived early to attend optional office hours before class.
+        This is indicative of his commitment to, and focus on, learning to code.
+        As his instructor, I could always count on him to not only not be
+        absent, but to be truly present. During class, Brian was always actively
+        engaged in completing the lesson’s activities, and his problem-solving
+        ability soon became apparent: he displayed the all-important skills of
+        knowing when to ask questions and knowing what questions to ask. Brian
+        also showed that he is a great collaborator with leadership ability, a
+        sense of humor, and the proper temperament to take on the challenges of
+        software development. He will be a valuable asset to any organization,
+        and I give him my full recommendation."
         <br></br>
-        
+        <strong> - Peter Colella, <em>Web Developer & Bootcamp Instructor.</em> </strong>
+        <br></br>
+        <br></br>
         “Using a consultant mindset, Brian takes great pride in finding creative
         solutions to complex problems. I have seen him work tirelessly to find a
         new angle or a new approach that will both meet the financial needs of
@@ -16,13 +32,37 @@ function Recommendations() {
         silver lining in any difficult situation. A depth of knowledge and
         real-life experience allows Brian to empathize with his customers and
         speak to them in a way that is unique and engaging. His ability to
-        deliver consistently strong results make him an asset to any team.” –
-        Jason Forgey, Program & Project Manager, CS Operations.<br></br>
+        deliver consistently strong results make him an asset to any team.”
         <br></br>
-        
+        <strong> – Jason Forgey, <em>Program & Project Manager.</em> </strong>
+        <br></br>
+        <br></br>
+        ​​”Brian has an innate ability to constantly think 2-3 steps ahead in
+        any situation, always adding value by compelling those he works with to
+        use this foresight to plan & prepare for their own success. I’ve worked
+        directly with Brian for the better part of five (5) years and have
+        personally experienced the dedication, pride and passion that he puts
+        into all he’s involved with (both inside & outside of work). Brian
+        remains a constant beacon of business knowledge, strategic insight &
+        personal integrity that any Team would be fortunate [to] incorporate.”
+        <br></br>
+        <strong> – Matt Mercer, <em>HR Business Partner.</em></strong>
+        <br></br>
+        <br></br>
       </div>
       <br></br>
       <br></br>
+      <a
+        href="https://www.credly.com/badges/a2526be3-fc55-45e8-8c4b-db81fb685808/linked_in?t=rm9jm1"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          className="badge1"
+          src={codeBadge1}
+          alt=""
+        />
+      </a>
     </div>
   );
 }
