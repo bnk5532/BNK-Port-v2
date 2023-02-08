195443b0e09f395c19d4117d124dfd2a6374e654
Brian King <bnk5532@gmail.com>
2023-02-07 22:24:59 -0500

    started building framework for photo grid

---
+++ src/components/Photo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import BVPrague2 from "../../assets/images/BVPrague2.png";
+
+
+function Photo(){
+    return{
+        //build html for photo grid
+
+    }
+}
+
+export default Photo;
+
