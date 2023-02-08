86467ea06019a63f1e166529b312f761bba496b2
Brian King <bnk5532@gmail.com>
2023-02-08 14:30:30 -0500

    html framework for photo grid built with filler image

--- src/components/Photo/index.js
+++ src/components/Photo/index.js
@@ -2,12 +2,69 @@ import React from "react";
 import BVPrague2 from "../../assets/images/BVPrague2.png";
 
 
-function Photo(){
-    return{
-        //build html for photo grid
 
-    }
+function Photo() {
+  return (
+    //build html for photo grid
+    <div className="album-container">
+      <div className="header" id="myHeader">
+        <h1>Image Grid</h1>
+        <p>Click on the buttons to change the grid view.</p>
+        <button className="btn" onclick="one()">
+          1
+        </button>
+        <button className="btn active" onclick="two()">
+          2
+        </button>
+        <button className="btn" onclick="four()">
+          4
+        </button>
+      </div>
+
+      <div className="row photo-grid">
+        <div className="column">
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+        </div>
+
+        <div className="column">
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+        </div>
+
+        <div className="column">
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+        </div>
+
+        <div className="column">
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+          <img src={BVPrague2} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Photo;
 
+
+
+
+
+export default Photo;
