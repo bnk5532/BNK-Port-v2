e582c863be2bbc05b35fce270957a7f8c067c540
Brian King <bnk5532@gmail.com>
2023-02-21 00:42:24 -0500

    minor content updates to modals and css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,53 +17,6 @@ import Project from "../Project";
 import Nav from "react-bootstrap/Nav";
 import Resume from "..//Resume";
 
-function MyDevOpsModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          <h3>
-            Talk Ops. Talk Tech. Do A Bit Of Everything. <em>Now be human!</em>
-          </h3>
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <h4>Why hire me as your DevOps Engineer?</h4>
-        <p>
-          Check out the diagram below and just a few highlights from my career and
-          surely you'll see why I am an excellent candidate for your next DevOps
-          opening.
-        </p>
-        <img className="d-block w-100" src={DevOpsGraph} alt="DevOps graphic" />
-        <h4>What is DevOps?</h4>
-        <p>
-          Broadly conceived, <strong>DevOps</strong> lives at the crossroads of
-          the development, operations, and quality assurance divisions of a
-          business and is of increasing importance as a company scales, drives
-          out costs, and becomes more reliant on the technology to meet client,
-          enterprise and stakeholder needs and expectations. A{" "}
-          <strong>DevOps Engineer</strong> (<em>like me</em>) helps to ensure a
-          seamless delivery on those priorities through a broader sense of the
-          business, a current and working knowledge of available technologies
-          and most especially, excellent communication and building of
-          cross-functional relationships. You might imagine a generalist of this
-          sort to be involved in everything from designing, building, testing,
-          deploying, automating, and owning projects, processes or applications
-          throughout their respective life cycles.
-        </p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
-}
-
 function MyWebDevModal(props) {
   return (
     <Modal
@@ -74,7 +27,8 @@ function MyWebDevModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Design. Build. Test. Deploy. <em>Always Be Coding!</em>
+          Design. Build. Test. Deploy. Repeat. <br></br>
+          <em>Always Be Coding!</em>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body className="web-dev-modal">
@@ -94,6 +48,62 @@ function MyWebDevModal(props) {
           anywhere else that you see the GitHub logo to access my other
           repositories.
         </Nav.Link>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function MyDevOpsModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          <h3>
+            Live Ops. Talk Tech. Do A Bit Of Everything Else. <br></br>
+            <em>Now be human!</em>
+          </h3>
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Why hire me as your DevOps Engineer?</h4>
+        <p>
+          <em>Well,</em> have a quick look at only a few highlights from my
+          career on the diagram below and I think that you'll have to agree that
+          I am an excellent candidate for your next DevOps opening.
+        </p>
+        <img
+          className="d-block w-100 pb-3"
+          src={DevOpsGraph}
+          alt="DevOps graphic"
+        />
+        <h4>What is DevOps?</h4>
+        <p>
+          Broadly conceived, <strong>DevOps</strong> lives at the crossroads of
+          the development, operations, and quality assurance divisions of a
+          business and is of increasing importance as a company scales, drives
+          out costs, and becomes more reliant on the technology to meet client,
+          enterprise and stakeholder needs and expectations. A{" "}
+          <strong>DevOps Engineer</strong> (<em>like me</em>) helps to ensure a
+          seamless delivery on those priorities through a broader sense of the
+          business, a current and working knowledge of available technologies
+          and most especially, excellent communication and building of
+          cross-functional relationships. You might imagine a generalist of this
+          sort to be involved in everything from designing, building, testing,
+          deploying, automating, and owning projects, processes or applications
+          throughout their respective life cycles.
+          {/* <br></br>
+          <br></br>
+          Dig into my experience, my credentials, reach out to my former leaders and teammates, and then let's start a conversation
+          - I think that you'll find that DevOps is exactly where I belong! */}
+        </p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -126,6 +136,19 @@ function MySrAnalystModal(props) {
   );
 }
 
+function AppWeb() {
+  const [modalShow, setModalShow] = React.useState(false);
+  return (
+    <>
+      <Button variant="btn-title p-0" onClick={() => setModalShow(true)}>
+        Web Developer &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+      </Button>
+
+      <MyWebDevModal show={modalShow} onHide={() => setModalShow(false)} />
+    </>
+  );
+}
+
 function AppDev() {
   const [modalShow, setModalShow] = React.useState(false);
   return (
@@ -139,18 +162,6 @@ function AppDev() {
   );
 }
 
-function AppWeb() {
-  const [modalShow, setModalShow] = React.useState(false);
-  return (
-    <>
-      <Button variant="btn-title p-0" onClick={() => setModalShow(true)}>
-        Web Developer &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-      </Button>
-
-      <MyWebDevModal show={modalShow} onHide={() => setModalShow(false)} />
-    </>
-  );
-}
 function AppSr() {
   const [modalShow, setModalShow] = React.useState(false);
   return (
@@ -259,8 +270,8 @@ function About() {
         <div className="about-me">
           {/* Web Developer &nbsp;|&nbsp; DevOps Engineer &nbsp;|&nbsp; Sr. Analyst  */}
           <span className="carousel">
-            <AppDev />
             <AppWeb />
+            <AppDev />
             <AppSr />
           </span>
           <br></br>
