c9bffb58c2204f348ef8e2c342e44ef4f383778c
Brian King <bnk5532@gmail.com>
2024-05-08 14:14:24 -0400

    updated CV pdf

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Collapse from "react-bootstrap/Collapse";
-import bnkCv from "../../assets/pdf/BNKCV2024v2.1.pdf";
+import bnkCv from "../../assets/pdf/BNKCV2024v2.2.pdf";
 import bnkCl from "../../assets/pdf/BNKGenCL2024v2.1.pdf";
 import grades from "../../assets/pdf/BootCampGrades.pdf";
 import codeBadge1 from "../../assets/images/codeBadge1.png";
